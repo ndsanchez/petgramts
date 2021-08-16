72732b672af6337ad4bdf152c975c709d9d3d7cd
Neil Sanchez <nd.sanchezq@gmail.com>
2021-08-16 14:13:11 -0500

    Typescript setup on webpack

---
+++ README.md
@@ -0,0 +1,1 @@
+# Petgram with Typescript
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.tsx',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      } 
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './public/index.html' })],
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx']
+  },
+  output: {
+    filename: 'bundle.js'
+  },
+}
