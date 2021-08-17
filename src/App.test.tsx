cf35cc6d71861110fd2bb599324d8689577ebc2a
Neil Sanchez <nd.sanchezq@gmail.com>
2021-08-16 21:21:13 -0500

    linted code

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import { render, fireEvent, waitFor, screen, getByTestId, getByText } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import App from './App'
 
