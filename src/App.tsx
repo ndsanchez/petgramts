51a41da1f754e3cb548fe6ce260d78c7cf693d1a
Neil David Sanchez Quintana <ndsanchezq@iMac-de-Neil.local>
2022-03-30 19:13:00 -0500

    Added Global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
 import Button from '@mui/material/Button'
 import ListOfCategories from './components/ListOfCategories'
+import { GlobalStyle } from './GlobalStyle'
 
 const App = (): JSX.Element => {
   const [btnText, setBtnText] = useState('Say hello!')
   return (
     <>
+      <GlobalStyle />
       <h1 id='gretting'>Welcome Neil!</h1>
       <ListOfCategories />
       <Button
